9934c00e11518073240a5243413ef94560be9ebe
BOUDEFFAZAKARYA <boudeffazack@gmail.com>
2023-05-23 00:57:25 +0100

    fix layout dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 
 import './globals.css'
+import { Inter } from 'next/font/google'
 import { Toaster } from "@/components/ui/toaster"
 import { NaviMenu } from './dashboard/navigation-menu'
 
 
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -18,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <body >
+      <body className={inter.className}>
 
 
         <div> {children}</div>
