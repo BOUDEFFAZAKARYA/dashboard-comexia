c550c8e6542b8f8f2189e7c84da05e2496d14131
BOUDEFFAZAKARYA <boudeffazack@gmail.com>
2023-06-04 08:45:31 +0100

    fuck me from the beginn

--- app/dashboard/kaywords/page.tsx
+++ app/dashboard/kaywords/page.tsx
@@ -3,23 +3,27 @@
 
 import { QueryClient, useHydrate, useQuery, useQueryClient } from "@tanstack/react-query"
 import axios from "axios"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import {  columns } from "./columns"
 import { DataTable } from "./data-table"
 
+import { usePathname, useRouter } from 'next/navigation'
 
-export type companies = {
-  
+
+
+export type offers = {
   id: number
-  name: string
-  countryCode:string
+  title: string
   desc: string
+  categorie: "import" | "export" 
+  countryCode:string
+  views:number
+  created_at:string
+  premium:boolean
   valide:boolean
   keywords: keyword
-  category :Category
 
 }
-
 
 
 export type keyword = {
@@ -28,103 +32,39 @@ export type keyword = {
   name:string
 }
 
-export type Category = {
+async function getData(): Promise<any> {
 
-  id:number
-  name:string
-}
-
-async function getkeywords(): Promise<any> {
-
-     const {data} = await axios.get('http://146.190.184.106:81/api/Keyword/All');
-
-     console.log("i'm here in getdata fct");
-
+     const {data} = await axios.get('http://146.190.184.106:81/api/Keyword/All     ')
 
       return data  
     
     } 
    
-export default  function KeywordPage() {
+export default async function Page() {
 
-
-
-  //const data = await getkeywords();
-
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
+  const pathname = usePathname();
 
   
-  const [isLoading, setisLoading] = useState(false);
-  useEffect(() => {
-
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://146.190.184.106:81/api/Keyword/All        ');
-        const jsonData = await response.json();
-        setData(jsonData);
-        setisLoading(false);
-      } catch (err) {
-        setError(error);
-        setisLoading(false);
-      }
-    };
-    fetchData();
+  console.log('Current endpoint:', pathname);
 
 
-  }, []);
- 
-  if (isLoading) {
-    return <div>Loading...</div>;
+  const data = await getData();
+
+  
+  if (!data) {
+    return <div>Loading...</div>
   }
 
-  if (error) {
-    return <div>Error</div>;
-  }
-  console.log(data); 
 
-  console.log("data");
-
-  const datastatic = [
-    {
-      "id": 1,
-      "name": "agricultural-machinery"
-    },
-    {
-      "id": 2,
-      "name": "baby-products"
-    },
-    {
-      "id": 3,
-      "name": "fitness"
-    },
-    {
-      "id": 4,
-      "name": "football"
-    },
-    {
-      "id": 5,
-      "name": "ticket"
-    },
-    {
-      "id": 8,
-      "name": "test2"
-    },
-    {
-      "id": 9,
-      "name": "test3"
-    },
-    {
-      "id": 10,
-      "name": "test3"
-    }
-  ]
 
   return (
     <div className="container mx-auto py-10">
-       <div>Data: {JSON.stringify(data)}</div>
+
+                  <DataTable columns={columns} data={data} /> 
+
+
+
 
     </div>
-
   )
 }
